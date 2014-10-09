d97f58b1b1cf94aa3f1bbb3a6591c5e6e365fc28
Vinay Venkatesh <vivenkat@ucsd.edu>
2014-10-08 23:40:54 -0700

    backend

--- UI/app/controllers/user.js
+++ UI/app/controllers/user.js
@@ -50,7 +50,6 @@ module.exports = function(_, io, participants, passport, refreshAllUsers) {
     	  res.redirect('/signup');
     	  return;
       }
-
       passport.authenticate('local-signup', function(err, user, isNewUser, info) {
         if (err)
           return next(err);
